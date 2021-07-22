3a36a5cc89aa3d951561870132e1bb08177425b4
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-07-22 17:35:38 +0100

    fix erorr,  major refactor

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
-﻿// C# implemenatation of above approach
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace UCS
-{
-  
+{  
     class GFG
     {
         static string CSV_DIR = Path.Combine(Directory.GetCurrentDirectory(),"Data");
         static bool DEBUG = true;   
-        static Dictionary <string,int> _nodes = new Dictionary<string,int> ();
-        static  List<Tuple<string,string,int>> graphandweight = new List<Tuple<string,string,int>> ();               
-        static (int,List<string>) uniformCost(string source, string goal) {
+        static Dictionary <string,int> _nodes = new Dictionary<string,int> ();        
+                   
+        static (int,List<string>) uniformCost(List<Tuple<string,string,int>> graph, string source, string goal) {
             //Define priority queue
             List<Tuple<int,string,string,int>> nodes_q = new List<Tuple<int, string, string, int> >();
             
@@ -26,13 +23,13 @@ namespace UCS
 
             //Intialised priority queue with start values
             //The tuple contains = (total_cost, current_node, prev_node, link_cost)
-            nodes_q.Add(new Tuple<int, string, string, int> (0,source, "-1",0));
+            nodes_q.Add(new Tuple<int, string, string, int> (0,source, "None",0));
             
             //Initialise all visited to false
             foreach (var node in _nodes)
             {
                 //Initialise all visited to false
-                visited_nodes[node.Value.ToString()] = false;
+                visited_nodes[node.Key] = false;
                 
                 //Set all nodes' values of previous node to null
                 prevousNodesAndCostsDictionary[node.Key] = null;
@@ -40,92 +37,97 @@ namespace UCS
 
             while (nodes_q.Count() != 0)
             {
-                // total_cost, current_node, prev_node, link_cost
-                
-                //Define current and set it to the top of queue value 
-                Tuple<int,string,string,int> current = nodes_q[0];
+                //Get the topOfQueue entry and set it to the top of queue value 
+                Tuple<int,string,string,int> topOfQueue = nodes_q[0];
 
-                //Pop the first element in priority queue
-                nodes_q.RemoveAt(0);
-                
                 //Set values from tuple
-                int total_cost = current.Item1;
-                string current_node = current.Item2;
-                string prev_node  =  current.Item3;
-                int link_cost = current.Item4;
+                int total_cost = topOfQueue.Item1;
+                string current_node = topOfQueue.Item2;
+                string prev_node  =  topOfQueue.Item3;
+                int link_cost = topOfQueue.Item4;
 
-                //if the current node has not been visited yet, do
+                /* Pop the first(top of queue) element from queue
+                   This is because the next block is about to process it
+                   and is shold no longer be at the top of the queue */
+                nodes_q.RemoveAt(0);
+
+                //if the current node has not been visited yet, do               
                 if(!visited_nodes[current_node])
                 {
-                    visited_nodes[current_node] = true;
-                    
-                    prevousNodesAndCostsDictionary[current_node] = new List<Tuple<string, int>> ();
-                    
-                    var tempTuple = new Tuple<string, int> (prev_node,link_cost);
+                    //set node as visited
+                    visited_nodes[current_node] = true; 
 
+                    //initialise new list
+                    prevousNodesAndCostsDictionary[current_node] = new List<Tuple<string, int>> (); 
+                    
+                    //Create a (prev_node,cost) tuple variable
+                    Tuple<string, int> tempTuple = new Tuple<string, int> (prev_node, link_cost);
+
+                    //Add (prev_node,cost) to current node's dictionary
                     prevousNodesAndCostsDictionary[current_node].Add(tempTuple);
-                    //log($" prevousNodesAndCostsDictionary[current_node] = { string.Join("|",prevousNodesAndCostsDictionary[current_node]) }");
 
-                    if(current_node == goal){
-                        List<string> final = new List<string> ();
+                    /* If the destination node has been reached, then do
+                       The results are returned in this block */
+                    if(current_node == goal)
+                    {                        
+                        List<string> finalPathList = new List<string> ();
                         while (current_node != source)
                         {                            
-                            List<string> temp = new List<string>(){};
+                            List<string> tempNodes = new List<string>(){};
                             foreach (var prevNodeAndCostList in prevousNodesAndCostsDictionary[current_node])
                             {
-                                //prevNodeAndCostList.Item1 is a node
-                                temp.Add(prevNodeAndCostList.Item1.ToString());
+                                //prevNodeAndCostList.Item1 is a node name string, it is added
+                                tempNodes.Add(prevNodeAndCostList.Item1.ToString());
                             }
-                            final.AddRange(temp);
+
+                            //Add the 'list' of nodes to the final list
+                            finalPathList.AddRange(tempNodes);
+                            
+                            /*
+                                Here take the first entry in the list of negihbors, picks the node name (Item1) 
+                                Set the current_node value to this node. 
+                                Doing so will, in the next iterations, pick up nodes, travelling back wards through 
+                                the uniform cost efficent path
+                            */
                             current_node = prevousNodesAndCostsDictionary[current_node][0].Item1.ToString();
-
-                        }                        
-                        final.Reverse();
-                        final.Add(goal);
-
-                        //Replace node index values to string names
-                        List<string> pathNodes = new List<string> ();
-                        foreach (var node in final)
-                        {
-                            //Find Dictionary key (string node name)  from string value (its integer index)
-                            var name = _nodes.FirstOrDefault(x => x.Value == (int.Parse(node))).Key;
-                            pathNodes.Add(name);                           
                         }
-                        return (total_cost, pathNodes);
+                        //Reverse order as nodes were appended from destination -> source            
+                        finalPathList.Reverse();
+
+                        //Add the name of the destination node
+                        finalPathList.Add(goal);
+
+                        return (total_cost, finalPathList);
                     }
 
                     List<Tuple<string,string,int>> currentNodeNeighbors = new List<Tuple<string, string, int>> ();
 
-                    foreach (var graphEntry in graphandweight)
-                    {
-                        if(graphEntry.Item1 == current_node){
-                            currentNodeNeighbors.Add(graphEntry);
-                        }                        
+                    /* Adds the current_node's neighbors from the graph*/
+                    foreach (var graphEntry in graph)
+                    {                        
+                        if(graphEntry.Item1 == current_node)
+                            currentNodeNeighbors.Add(graphEntry);                        
                     }
 
-
+                    //log($"currentNodeNeighbors = {string.Join("|",currentNodeNeighbors)}");
                     foreach (var tuple in currentNodeNeighbors)
                     {
-                        var neighbor = tuple.Item2;
-                        var ncost = tuple.Item3;
+                        string neighbor = tuple.Item2;
+                        int ncost = tuple.Item3;
 
                         if(!visited_nodes[neighbor]){
-                            //set this code 
+                            //set this cost from this node to its negihbor 
                             int this_link_cost = ncost;
 
-                            //calculate new cost
+                            //calculate new cost FROM source
                             int new_cost = total_cost + ncost;
 
-                            //new_cost, neighbors, current_node, ncost
+                            //Add to the computed values, prev & current nodes to the queue
                             nodes_q.Add(new Tuple<int, string, string, int> (new_cost, neighbor, current_node, ncost)); 
-                        }
-                        
+                        }                        
                     }
-
                 }
-
             }
-
 
             return (0, new List<string>());
         }
@@ -134,7 +136,7 @@ namespace UCS
                 Console.WriteLine(_input);
         }
 
-
+        /// Returns the graph connection and weight values from csv file. 
         public static List<Tuple<string,string,int>> GetGraphAndWeight(string fileNameWithExtension) {
             List<Tuple<string,string,int>>gnw = 
                 new List<Tuple<string,string,int>> ();
@@ -151,6 +153,7 @@ namespace UCS
             return gnw;
         }
 
+        ///Returns the Node names and indices.
         public static Dictionary<string, int> GetNodes(string fileNameWithExtension)
         {
             Dictionary<string, int> dict = new Dictionary<string, int> ();
@@ -160,34 +163,36 @@ namespace UCS
             lines.ForEach(x => dict.Add(x[0], int.Parse(x[1])));
 
             return dict;
-        }
+        }        
 
         // main function
         public static void Main(params string []args)
         {        
-            //Read node names from file
-            _nodes = GetNodes("nodes_1.csv");
+            List<Tuple<string,string,int>> graphandweight = new List<Tuple<string,string,int>> ();
+            /* Read node names from file*/
+            //_nodes = GetNodes("nodes_1.csv");
+            _nodes = GetNodes("taxidrivernodes.csv");            
             Console.WriteLine($"file nodes: {string.Join("--",_nodes.ToList())}");          
 
-            //Read graph and cost from file
-            graphandweight= GetGraphAndWeight("cost_1.csv");
+            /* Read graph connections and cost from file */
+            //graphandweight= GetGraphAndWeight("cost_1.csv");
+            graphandweight = GetGraphAndWeight("taxidrivergraph.csv");
 
-            //Console INput values
+            //Fetch Console Input values
             Console.Write("Enter name of START node = ");
-            string _start = Console.ReadLine();
+            string _start = Console.ReadLine().Trim();
 
             Console.Write("Enter name of END node = ");
-            string _end = Console.ReadLine();            
+            string _end = Console.ReadLine().Trim();  
 
-            int start = (_nodes[_start]);
-            int end = _nodes[_end];
+            Console.WriteLine($"Calculating uniform search from (start, end) = ({_start},{_end})...");
 
-            Console.WriteLine($"Calculating uniform search from (start, end) = ({start},{end})...");        
+            //Execute uniform cost search
+            (int totalCost, List<string> pathTaken) =  uniformCost(graphandweight, _start, _end);
 
-            (int totalCost, List<string> pathTaken) =  uniformCost(start.ToString(), end.ToString());
+            //Display uniform cost search results
             Console.WriteLine($"uniformCost:: totalCost :{totalCost}");
             Console.WriteLine($"uniformCost:: pathTaken :{string.Join("--->", pathTaken)}");
-
 
             Console.ReadLine();
         }
